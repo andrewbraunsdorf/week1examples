d931e4f72838fca12ca657aa0a8fef8cce3cef34
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-05-02 23:51:09 +0000

    boolean for is a leap year

--- challengeWeek1.c
+++ challengeWeek1.c
@@ -8,35 +8,65 @@
 #include <cs50.h>
 #include <stdio.h>
 
+bool isLeapYear(int);
+
 int main(void)
 {
-    int a;
-    printf("Enter the first year:");
-    scanf("%d", &a);
-        if (a % 400 == 0)
-        {
-            printf("Leap year\n");
-        }
-
-        else if (a % 100 == 0)
-        {
-            printf("NOT A Leap year\n");
-        }
-
-        else if (a % 4 == 0)
-        {
-            printf("Leap year\n");
-        }
-         else
-        {
-            printf("NOT A Leap year\n");
-        }
+    int year = get_int("Enter a year: ");
+    if (isLeapYear(year))
+    {
+        printf("%i is a leap year.\n", year);
+    } else {
+        printf("%i is NOT a leap year.\n", year);
+    }
 }
 
-bool leapYear()
+bool isLeapYear(year)
 {
-
+    if(year % 4 == 0)
+    {
+        if(year % 100 == 0)
+        {
+            if(year % 400 ==0)
+            {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    return false;
 }
+
+// int main(void)
+// {
+//     int a;
+//     printf("Enter the first year:");
+//     scanf("%d", &a);
+//         if (a % 400 == 0)
+//         {
+//             printf("Leap year\n");
+//         }
+
+//         else if (a % 100 == 0)
+//         {
+//             printf("NOT A Leap year\n");
+//         }
+
+//         else if (a % 4 == 0)
+//         {
+//             printf("Leap year\n");
+//         }
+//          else
+//         {
+//             printf("NOT A Leap year\n");
+//         }
+// }
+
+// bool leapYear()
+// {
+
+// }
 
 // if mod == 4 return leap year
 // if mod !== 4 return NOT leap year
